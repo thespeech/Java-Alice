5b88c855d2e81f7e3a5db5034eb30322879241e4
thespeech <dominaria@gmail.com>
2016-08-28 23:33:57 +0800

    Just added memory map

--- Something About Alice/src/com/alice/core/Brain.java
+++ Something About Alice/src/com/alice/core/Brain.java
@@ -10,4 +10,5 @@ public class Brain {
 	private List<Trait> traits;
 	private List<Belief> beliefs;
 	
+	
 }
--- Something About Alice/src/com/alice/core/Memory.java
+++ Something About Alice/src/com/alice/core/Memory.java
@@ -1,10 +1,17 @@
 package com.alice.core;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Memory {
 
 	private int id; //Id of the memory type - related to event type
+	private int age; //Days since memory.
 	List<Integer> idsOfPeopleInvolved; //For quick reference.
-	List<String> parameters; //Since it varies on each memory, must create 
+	Map memoryMap; //Since it varies on each memory, must create
+	
+	public Memory() {
+		memoryMap = new HashMap<String, String>(); //Flexible based on events.
+	}
 }
